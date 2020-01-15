2480013e1845df1237b96265a61b24da696e707f
Sammii-HK <sammii.h@icloud.com>
2020-01-15 23:27:36 +0000

    altering data set

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import rd3 from 'react-d3-library'
+import * as d3 from 'd3'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App () => {
+
+  // componentDidMount() {
+  //   const data = [12, 5, 6, 6, 9, 10];
+  //
+  //   const svg = d3.select("body").append("svg").attr("width", 700).attr("height", 300);
+  //
+  //   svg.selectAll("rect").data(data).enter().append("rect")
+  //
+  //   svg.selectAll("rect")
+  //   .data(data)
+  //   .enter()
+  //   .append("rect")
+  //   .attr("x", (d, i) => i * 70)
+  //   .attr("y", 0)
+  //   .attr("width", 25)
+  //   .attr("height", (d, i) => d)
+  //   .attr("fill", "green");
+  //
+  // }
+
+  const [books, setBooks] = useState(initialBooks)
+  const initialBooks = [
+    {
+        name: "Harry Potter and the Philosophers Stone",
+        author: "J. K. Rowling",
+        genre: "fantasy"
+    },{
+        name: "The Pedagogy of Freedom",
+        author: "Bell hooks",
+        genre: "non-fiction"
+    },{
+        name: "Harry Potter and the Chamber of Secrets",
+        author: "J. K. Rowling",
+        genre: "fantasy"
+    },{
+        name: "Gilgamesh",
+        author: "Derrek Hines",
+        genre: "poetry"
+    }
+]
+
+  render() {
+    return (
+      <div className="App">
+        <h1>I'm here 😎</h1>
+        <div id={"#"}></div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+// let data
+// const nodes = []
+// const throttledNodes = []
+// let graph
+// const width = 800
+// const height = 800
+// const throttleIndex = 0
+// const throttleNum = 10
+// let timer
+// const labels = []
+// const color = d3.scale.catergory20()
+// d3.request('/assets/significantvolcanoeruptions.csv')
+//   .mimeType('text/csv')
+//   .response(xhr => d3.csvParse(xhr.responseText))
+//   .get(data => {
+//     this.setState({data})
+//   })
+
+// d3.csv('/assets/significantvolcanoeruptions.csv')
+//   .row(function(d) { return {key: d.key, value: +d.value} })
+//   .get (function(error, rows) { console.log("rows", rows) })
