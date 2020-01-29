c803e5eb344239e67cbe7b69e4e70970484fb659
Sammii-HK <sammii.h@icloud.com>
2020-01-29 17:49:17 +0000

    setting graph bounds with scaleLiner

--- public/main.js
+++ public/main.js
@@ -1,16 +1,17 @@
 let dataset
-let w = 500;
-let h = 100;
+let w = 500
+let h = 100
 
-let nodes = [];
-let throttledNodes = [];
-let graph;
+let nodes = [] // To push extracted data from dataset to
 
-let throttleIndex = 0;
-let throttleNum = 10;
-let timer;
-let labels = [];
-let color = d3.scale.category20();
+let throttledNodes = []
+let graph
+
+let throttleIndex = 0
+let throttleNum = 10
+let timer
+let labels = []
+let color = d3.scale.category20()
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JS loaded')
@@ -24,14 +25,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	})
 
 
-	// //Create scale functions
-	// var xScale = d3.scaleLinear()
-	// 					 .domain([0, d3.max(dataset, function(d) { return d[0]; })])
-	// 					 .range([0, w]);
-  //
-	// var yScale = d3.scaleLinear()
-	// 					 .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-	// 					 .range([0, h]);
 
 
   // // set up animation and apply some gravity rules
@@ -39,50 +32,66 @@ document.addEventListener('DOMContentLoaded', () => {
   // .gravity(0.04)
   // .charge(0)
   // .nodes(throttledNodes)
-  // .size([w, h]);
+  // .size([w, h])
 
   // // on every layout tick, check for collisions using quadtree spacial subdivisions
   // force.on("tick", function(e) {
   //   var q = d3.geom.quadtree(throttledNodes),
   //   i = 0,
-  //   n = throttledNodes.length;
+  //   n = throttledNodes.length
   //
   //   while (i < n) {
-  //     q.visit(collide(throttledNodes[i]));
-  //     i++;
+  //     q.visit(collide(throttledNodes[i]))
+  //     i++
   //   }
   // })
 
 
 let visualiseData = () => {
 
+  var linearScale = d3.scaleLinear()
+    .domain([0, 10])
+    .range([0, 600]);
+
+	//Create scale functions
+	var xScale = d3.scaleLinear()
+						 .domain([-180, 180])
+						 .range([0, w])
+
+	var yScale = d3.scaleLinear()
+						 .domain([-90, 90])
+						 .range([0, h])
+  //
+	// var yScale = d3.scaleLinear()
+	// 					 .domain([0, d3.max(dataset, function(d) { return d.long })])
+	// 					 .range([0, h])
+
   // Extract from dataset
   dataset.forEach((val, i, array) => {
     let node = {}
-    node.date = new Date(val['Year'], 1, 1, 0, 0, 0, 0);
-    node.type = val['Type'];
-    node.lat = val['Latitude'];
-    node.long = val['Longitude'];
-    if ('' == node.type) node.type = 'Type N/A';
-    node.amount = val['Volcano Explosivity Index (VEI)'];
-    node.radius = node.amount * 2;
-    nodes.push(node);
-    if (labels.indexOf(node.type) == -1) labels.push(node.type);
+    node.date = new Date(val['Year'], 1, 1, 0, 0, 0, 0)
+    node.type = val['Type']
+    node.lat = val['Latitude']
+    node.long = val['Longitude']
+    if ('' == node.type) node.type = 'Type N/A'
+    node.radius = val['Volcano Explosivity Index (VEI)']
+    nodes.push(node)
+    if (labels.indexOf(node.type) == -1) labels.push(node.type)
   })
   console.log("nodes:", nodes)
   // Sort nodes by date
-  nodes.sort(function(a, b) {return a.date - b.date;});
+  nodes.sort(function(a, b) {return a.date - b.date})
 
   //Create SVG element
   var svg = d3.select("body")
   .append("svg")
   .attr("width", w)
-  .attr("height", h);
+  .attr("height", h)
 
   // // Apply new properties to circles
   // svg.selectAll(".data-circle")
-  // .attr("cx", function(d) { return d.lat; })
-  // .attr("cy", function(d) { return d.long; });
+  // .attr("cx", function(d) { return d.lat })
+  // .attr("cy", function(d) { return d.long })
 
   // Create circles
   svg.selectAll(".data-circle")
@@ -90,19 +99,19 @@ let visualiseData = () => {
      .enter()
      .append("circle")
      .attr("cx", function(d) {
-        return parseInt(d.lat);
+        return parseInt(d.lat)
      })
      .attr("cy", function(d) {
-        return parseInt(d.long);
+        return parseInt(d.long)
      })
      // .attr("r", function(d) {
-     //        return 10;
-     //     });
+     //        return 10
+     //     })
      .attr("r", function(d) {
-     		return Math.sqrt(h - d.radius);
+     		return Math.sqrt(h - d.radius)
      })
      .attr("class", function(d) {
-       return "data-circle";
+       return "data-circle"
      })
 }
 
@@ -116,32 +125,32 @@ let visualiseData = () => {
   //   nx1 = node.x - r,
   //   nx2 = node.x + r,
   //   ny1 = node.y - r,
-  //   ny2 = node.y + r;
+  //   ny2 = node.y + r
   //   return function(quad, x1, y1, x2, y2) {
   //     if (quad.point && (quad.point !== node)) {
   //       var x = node.x - quad.point.x,
   //       y = node.y - quad.point.y,
   //       l = Math.sqrt(x * x + y * y),
-  //       r = node.radius + quad.point.radius;
+  //       r = node.radius + quad.point.radius
   //       if (l < r) {
-  //         l = (l - r) / l * .5;
-  //         x *= l;
-  //         y *= l;
-  //         node.x -= x;
-  //         node.y -= y;
-  //         quad.point.x += x;
-  //         quad.point.y += y;
+  //         l = (l - r) / l * .5
+  //         x *= l
+  //         y *= l
+  //         node.x -= x
+  //         node.y -= y
+  //         quad.point.x += x
+  //         quad.point.y += y
   //       }
   //     }
-  //     return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-  //   };
+  //     return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1
+  //   }
   // }
 //
 // let addCircles = () => {
 //   // trickle nodes in. tweak throttleNum vars to alter timing
 //   let adds = nodes.slice(throttleIndex, throttleIndex += throttleNum)
 //
-//   throttledNodes = throttledNodes.concat(adds);
+//   throttledNodes = throttledNodes.concat(adds)
 //
 //   // Create circles
 //   svg.selectAll(".data-circle")
@@ -149,34 +158,34 @@ let visualiseData = () => {
 //      .enter()
 //      .append("circle")
 //      // .attr("cx", function(d) {
-//      //    return parseInt(d.Latitude);
+//      //    return parseInt(d.Latitude)
 //      // })
 //      // .attr("cy", function(d) {
-//      //    return parseInt(d.Longitude);
+//      //    return parseInt(d.Longitude)
 //      // })
 //      // .attr("r", function(d) {
-//      //        return 10;
-//      //     });
+//      //        return 10
+//      //     })
 //      .attr("r", function(d) {
-//      		return Math.sqrt(h - d.radius);
+//      		return Math.sqrt(h - d.radius)
 //      })
 //      .attr("class", function(d) {
-//        return "data-circle";
+//        return "data-circle"
 //      })
 //
 //
 //      // if (throttledNodes.length >= nodes.length) {
-//      //    clearInterval(timer);
+//      //    clearInterval(timer)
 //      //  }
 //      //
-//      //  force.nodes(throttledNodes);
+//      //  force.nodes(throttledNodes)
 //      //
-//      //  force.start();
+//      //  force.start()
 //
 //
 // }
 
 // start adding circles
-  // timer = setInterval(addCircles, 100);
+  // timer = setInterval(addCircles, 100)
 
 })
