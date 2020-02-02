78f6551ec55065f4c58d0eea870d578d9ddced71
Sammii-HK <sammii.h@icloud.com>
2020-02-02 21:51:13 +0000

    removed unnecessary code

--- public/main.js
+++ public/main.js
@@ -56,32 +56,7 @@ var projection = d3.geoEquirectangular()
     					.attr("height", h)
 
 
-  // // set up animation and apply some gravity rules
-  // var force = d3.layout.force()
-  // .gravity(0.04)
-  // .charge(0)
-  // .nodes(throttledNodes)
-  // .size([w, h])
-
-  // // on every layout tick, check for collisions using quadtree spacial subdivisions
-  // force.on("tick", function(e) {
-  //   var q = d3.geom.quadtree(throttledNodes),
-  //   i = 0,
-  //   n = throttledNodes.length
-  //
-  //   while (i < n) {
-  //     q.visit(collide(throttledNodes[i]))
-  //     i++
-  //   }
-  // })
-
-
 let visualiseData = () => {
-
-  // var linearScale = d3.scale
-  //   .linear()
-  //   .domain([0, 10])
-  //   .range([0, 600]);
 
 	//Create scale functions
 	var xScale = d3.scale
@@ -119,12 +94,6 @@ let visualiseData = () => {
   console.log("nodes:", nodes)
   // Sort nodes by date
   nodes.sort(function(a, b) {return a.date - b.date})
-
-  // //Create SVG element
-  // var svg = d3.select("body")
-  // .append("svg")
-  // .attr("width", w)
-  // .attr("height", h)
 
   // // Apply new properties to circles
   // svg.selectAll(".data-circle")
@@ -182,78 +151,5 @@ let visualiseData = () => {
 
     }
 
-
-
-
-  // // handle collisions
-  // // visit http://bl.ocks.org/mbostock/3231298 for more info
-  // var collide = function (node) {
-  //
-  //   var r = node.radius + 16,
-  //   nx1 = node.x - r,
-  //   nx2 = node.x + r,
-  //   ny1 = node.y - r,
-  //   ny2 = node.y + r
-  //   return function(quad, x1, y1, x2, y2) {
-  //     if (quad.point && (quad.point !== node)) {
-  //       var x = node.x - quad.point.x,
-  //       y = node.y - quad.point.y,
-  //       l = Math.sqrt(x * x + y * y),
-  //       r = node.radius + quad.point.radius
-  //       if (l < r) {
-  //         l = (l - r) / l * .5
-  //         x *= l
-  //         y *= l
-  //         node.x -= x
-  //         node.y -= y
-  //         quad.point.x += x
-  //         quad.point.y += y
-  //       }
-  //     }
-  //     return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1
-  //   }
-  // }
-//
-// let addCircles = () => {
-//   // trickle nodes in. tweak throttleNum vars to alter timing
-//   let adds = nodes.slice(throttleIndex, throttleIndex += throttleNum)
-//
-//   throttledNodes = throttledNodes.concat(adds)
-//
-//   // Create circles
-//   svg.selectAll(".data-circle")
-//      .data(nodes)
-//      .enter()
-//      .append("circle")
-//      // .attr("cx", function(d) {
-//      //    return parseInt(d.Latitude)
-//      // })
-//      // .attr("cy", function(d) {
-//      //    return parseInt(d.Longitude)
-//      // })
-//      // .attr("r", function(d) {
-//      //        return 10
-//      //     })
-//      .attr("r", function(d) {
-//      		return Math.sqrt(h - d.radius)
-//      })
-//      .attr("class", function(d) {
-//        return "data-circle"
-//      })
-//
-//
-//      // if (throttledNodes.length >= nodes.length) {
-//      //    clearInterval(timer)
-//      //  }
-//      //
-//      //  force.nodes(throttledNodes)
-//      //
-//      //  force.start()
-//
-//
-// }
-
-// start adding circles
-  // timer = setInterval(addCircles, 100)
 
 })
