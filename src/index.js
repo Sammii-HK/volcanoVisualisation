c54c45996d34409493b59cba6a8e2ddb5458be18
Samantha Kellow <sammii.h@icloud.com>
2021-12-05 22:30:50 +0000

    refactoring d3 index.js

--- src/index.js
+++ src/index.js
@@ -6,31 +6,42 @@ const config = {
   horizontalTilt: 0
 }
 let locations = []
-
-document.addEventListener('DOMContentLoaded', () => {
-console.log('JS loaded')
-
-const svg = d3.select('body')
-              .append('svg')
-              .attr('width', width)
-              .attr('height', height)
-              .attr('class', 'container')
-
-const markerGroup = svg.append('g')
 const projection = d3.geoOrthographic()
-const initialScale = projection.scale()
 const path = d3.geoPath().projection(projection)
+const initialScale = projection.scale()
+let svg
+let markerGroup
 const center = [width/2, height/2]
 
+document.addEventListener('DOMContentLoaded', () => {
+  svg = d3.select('body')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .attr('class', 'container')
+  
+  markerGroup = svg.append('g')
 
+  svg.call(d3.drag()
+    .on("drag", function() {
+      var xy = d3.mouse(this);
+      projection.rotate(xy)
+      svg.selectAll("path")
+        .attr("d",path);
+      drawMarkers();
+    })
+  )
+  
   drawGlobe()
   drawGraticule()
-  // enableRotation()
+  console.log('JS loaded')
+})
 
 function drawGlobe() {
   d3.queue()
       .defer(d3.json, 'https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-110m.json')
       .defer(d3.json, '/data/locations.json')
+      // .defer(d3.csv, '/data/significantvolcanoeruptions.csv')
       .await((error, worldData, locationData) => {
         svg.selectAll(".segment")
           .data(topojson.feature(worldData, worldData.objects.countries).features)
@@ -74,35 +85,27 @@ function drawGraticule() {
 //     })
 // }
 
-svg.call(d3.drag()
-    .on("drag", function() {
-      var xy = d3.mouse(this);
-      projection.rotate(xy)
-      svg.selectAll("path")
-        .attr("d",path);
-        // drawGlobe();
-        drawMarkers();
-    }))
+
 
 function drawMarkers() {
-    const markers = markerGroup.selectAll('circle')
-        .data(locations)
-    markers
-        .enter()
-        .append('circle')
-        .merge(markers)
-        .attr('cx', d => projection([d.longitude, d.latitude])[0])
-        .attr('cy', d => projection([d.longitude, d.latitude])[1])
-        .attr('fill', d => {
-            const coordinate = [d.longitude, d.latitude]
-            gdistance = d3.geoDistance(coordinate, projection.invert(center))
-            return gdistance > 1.57 ? 'none' : 'steelblue'
-        })
-        .attr('r', 7)
-
-    markerGroup.each(function () {
-        this.parentNode.appendChild(this)
+  const markers = markerGroup.selectAll('circle')
+    .data(locations)
+  markers
+    .enter()
+    .append('circle')
+    .merge(markers)
+    .attr('cx', d => projection([d.longitude, d.latitude])[0])
+    .attr('cy', d => projection([d.longitude, d.latitude])[1])
+    .attr('fill', d => {
+        const coordinate = [d.longitude, d.latitude]
+        gdistance = d3.geoDistance(coordinate, projection.invert(center))
+        return gdistance > 1.57 ? 'none' : 'steelblue'
     })
+    .attr('r', 7)
+
+  markerGroup.each(function () {
+    this.parentNode.appendChild(this)
+  })
 }
 
 })
